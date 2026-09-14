50ea04213d3be663383b8f0a3fc43d60edab365c
RichardZuidam <rzuidm@hotmail.com>
2026-09-14 14:07:06 +0200

    Integrate Vietnamese frequency lists

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE='loop-v1';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg'];
+const CACHE='loop-v2';
+const ASSETS=['./','index.html','styles.css','frequency-data.js','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
